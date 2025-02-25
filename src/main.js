9c18b6948ee9a271e108ab18df7d98dcb5a1e0a2
maskence <tqt>
2025-02-25 22:11:46 +0300

    added a console log

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import { getBalance } from 'arweavekit/wallet'
 async function get_ar_balance() {
     if (! await ArConnect.isInstalled()) return -1
   const response = await ArConnect.connect({permissions :['ACCESS_ADDRESS']});  
-  console.log(response,'========')  
   const address = await ArConnect.getActiveAddress();
   const balance = await getBalance({address : address, options : {winstonToAr : true}})
   return balance
@@ -15,6 +14,7 @@ async function get_ar_balance() {
 async function show_bags() {
 
     let balance = await get_ar_balance()
+    console.log('AR balance :', balance)
 
     const weagger_quote = `- "${Math.floor(balance)} AR ? That's a certified Weagger if i've ever seen one ! Come on in my friend, you are welcome here !"`
     const light_bag_quote = `- "${Math.floor(balance)} AR ? These bags seem a bit light my friend. Im sorry but we dont let lowcoiners in there. I will give you one more chance to accumulate, come back here when you have 100 AR or more"`
